c3c141144f9d1d16d35a3c96c44f96ae93ad217b
laucavv <laucavv23@gmail.com>
2020-04-30 23:52:43 +0000

    my own calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    operacion = ['+', '-', '*', '/']
+    len_argv = len(sys.argv)
+    if len_argv != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b> ")
+        exit(1)
+    if sys.argv[2] in operacion:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    if sys.argv[2] == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif sys.argv[2] == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif sys.argv[2] == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    else:
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
