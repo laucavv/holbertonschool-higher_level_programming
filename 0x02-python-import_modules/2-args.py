f53101813f2908cdef1e3cc4e3e6eb1ab30a28e5
laucavv <laucavv23@gmail.com>
2020-04-30 21:15:32 +0000

    fix args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     import sys
     len_argv = len(sys.argv)
-    if len_argv > 1:
-        print("{:d} argumentos:".format((len_argv - 1))
-        for i in range(1, len_argv):
-            print("{:d}: {:s}".format(i, sys.argv[i]))
-    else:
+    if len_argv == 1:
         print("0 arguments.")
+    elif len_argv == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(len_argv - 1))
+        for i in range(1, len_argv):
+            print("{:d}: {}".format(i, sys.argv[i]))
