deb7f36f8e3dfe888d037b03f5b5aeee10efacc0
laucavv <laucavv23@gmail.com>
2020-05-07 23:01:24 +0000

    best

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        i = 0
+        for key, value in a_dictionary.items():
+            
+            if i < value:
+                i = value
+                b = key
+        return b
+    return None
