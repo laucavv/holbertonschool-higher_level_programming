9e2aa557a3e6f420403f0a2e4ea22a93be965663
laucavv <laucavv23@gmail.com>
2020-05-07 21:11:25 +0000

    fix

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhon3
-def update_dictionary(a_dictionary, key, value):
+#!/usr/bin/python3
+ief update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
