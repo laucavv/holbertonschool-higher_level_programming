b3df233b1a8152142454042630be2e8c5a5edd83
laucavv <laucavv23@gmail.com>
2020-05-07 23:04:17 +0000

    fix pep8

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,6 @@ def best_score(a_dictionary):
     if a_dictionary:
         i = 0
         for key, value in a_dictionary.items():
-            
             if i < value:
                 i = value
                 b = key
