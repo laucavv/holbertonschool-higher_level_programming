c9446b0dc0b4bc89d8547cc3ee70ed2bc1df1d90
laucavv <laucavv23@gmail.com>
2020-05-08 03:49:11 +0000

    roman_2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    summ = 0
     if roman_string and type(roman_string) is str:
         num_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        idx = 0
-        for idx, item in enumerate(roman_string):
-            for key, value in num_roman.items():
-                if item == key and idx > 0:
-                    if num_roman[roman_string[idx]] > num_roman[roman_string[idx-1]]:
-                        summ = summ + value - num_roman[roman_string[idx-1]]
-                    else:
-                        summ = summ + value
-                else:
-                    summ = summ + value
-    return summ
-
+        summ, item = 0, 0
+        for i in roman_string:
+            if num_roman[i] > item:
+                summ += num_roman[i] - item * 2
+            else:
+                summ += num_roman[i]
+            item = num_roman[i]
+            return summ
+    return 0
