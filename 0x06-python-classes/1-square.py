bc6a89ea1f9b2bc97e936dc95388c6db1a3260fe
laucavv <laucavv23@gmail.com>
2020-05-19 23:09:38 +0000

    size_square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" create an class called square with size"""
+
+
+class Square:
+    """ init method to initialize"""
+    def __init__(self, size):
+        self.__size = size
