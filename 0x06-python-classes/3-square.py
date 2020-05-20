151087eb1150c5bdfe9fa872dc2c293516bd2809
laucavv <laucavv23@gmail.com>
2020-05-20 00:17:15 +0000

    calculate area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" create an class called square with size and we validate the input """
+
+
+class Square:
+    """init method to initialize in constructor"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """calculate the area of the square"""
+        return self.__size * self.__size
