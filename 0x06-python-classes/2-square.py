d3ae18900ea16c643ca07ba67cd1a8ae8a88b114
laucavv <laucavv23@gmail.com>
2020-05-20 00:02:04 +0000

    square whit validate

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" create an class called square with size and we validate the input """
+
+
+class Square:
+    """init method to initialize in constructor"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
