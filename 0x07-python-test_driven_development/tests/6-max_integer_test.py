76b349722de62483da4de32ae5a82f7aed2ad160
laucavv <laucavv23@gmail.com>
2020-05-23 06:08:17 +0000

    fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-    Unittest
+"""Unittest for max_integer([..])
 """
 
 import unittest
@@ -8,15 +7,20 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+    """ Class testing
     """
-        Testing function
-    """
-    def test_max_integer(self):
-        self.assertEqual(max_integer([1, 2, 3]), 3)
-        self.assertEqual(max_integer([-5, -30, -15, -23]), -5)
-        self.assertEqual(max_integer([3.4, -2.1]), 3.4)
-        self.assertEqual(max_integer([5, 5, 5, 5]), 5)
+    def testing(self):
+        self.assertEqual(max_integer([1, 3, 4, 56, 78, 9]), 78)
+        self.assertEqual(max_integer([-80, 80, 92, 99]), 99)
+        self.assertEqual(max_integer([1.190, 78.2, 6.3, float(1)]),
+                         float(78.2))
+        self.assertEqual(max_integer([-1000, 1]), 1)
+        self.assertEqual(max_integer([0, 0, 0, 0]), 0)
+        self.assertEqual(max_integer([-1, -2, -3, -4, -5]), -1)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([-98.5, -7.4]), -7.4)
+        self.assertEqual(max_integer([1.1, 1.2, 1.3, 1.4]), 1.4)
         self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([89]), 89)
-        self.assertEqual(max_integer([1.3, 23, 10, 90.1, 90.2]), 90.2)
         self.assertEqual(max_integer([-10]), -10)
+        self.assertEqual(max_integer([1.1, 1, 2.2, 2]), 2.2)
+        self.assertEqual(max_integer([3/2, 1, 0.4, 1-0]), 3/2)
