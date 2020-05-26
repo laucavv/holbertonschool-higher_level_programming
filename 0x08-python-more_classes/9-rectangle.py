b8e6b4edc16316a0ae57bd22377633e65210906b
laucavv <laucavv23@gmail.com>
2020-05-26 00:11:11 +0000

    fix

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -19,8 +19,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """init method to initialize in constructor"""
-        self.height = height
-        self.width = width
+        self.__height = height
+        self.__width = width
         Rectangle.number_of_instances += 1
 
     @property
