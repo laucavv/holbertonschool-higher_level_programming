69e83e70d111cc1f33b738ba6276afbcd879d7c6
laucavv <laucavv23@gmail.com>
2020-06-03 23:05:25 +0000

    fix name funtion

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -2,7 +2,7 @@
 """function that appends a string at the end of a text file """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """ funtion """
     with open(filename, mode="a", encoding="utf-8") as file:
         return file.write(text)
