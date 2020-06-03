7a4acee98a6a62bea8811ce14d524dfb364c8698
laucavv <laucavv23@gmail.com>
2020-06-03 03:49:02 +0000

    squarex2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" class Square """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class"""
+
+    def __init__(self, size):
+        """ builder """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+        self.area()
+        
+    def __str__(self):
+        """ str """
+        return "[Square] {}/{}".format(self.__size, self.__size)
