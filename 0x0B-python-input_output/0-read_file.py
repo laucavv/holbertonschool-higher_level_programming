4542092ee895751caa1bc314460c275e7f934afe
laucavv <laucavv23@gmail.com>
2020-06-03 20:17:44 +0000

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" function that reads a text file (UTF8)
+    and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """ read file"""
+    with open("my_file_0.txt", mode="r", encoding="utf-8") as my_file:
+        print(my_file.read(), end="")
