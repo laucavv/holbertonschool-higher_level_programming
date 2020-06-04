4ddf1a4cad22a18974e5ff3ea6f78a3b06f3e55a
laucavv <laucavv23@gmail.com>
2020-06-04 03:50:39 +0000

    12-student

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" class Student """
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        "builder"
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        """
+            method that retrieves a dictionary representation
+            of a Student instance
+        """
+        if attr is None:
+            return self.__dict__
+
+        my_dic = {}
+
+        for elements in attr:
+            if elements in self.__dict__:
+                my_dic[elements] = self.__dict__[elements]
+
+        return my_dic
