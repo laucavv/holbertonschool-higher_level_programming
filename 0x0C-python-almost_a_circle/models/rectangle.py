325d478db22e7043cc2009632f949fd0e2426451
laucavv <laucavv23@gmail.com>
2020-06-06 22:21:31 +0000

    update rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ Class Rectangle that inherits from Base """
 
+
 from models.base import Base
+
 
 class Rectangle(Base):
     """
@@ -24,49 +26,49 @@ class Rectangle(Base):
     def width(self):
         """ Getter """
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """ Setter """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("width must be > 0")  
+            raise ValueError("width must be > 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """ Getter """
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """ Setter """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("height must be > 0")  
-        self.__height= value
+            raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
         """ Getter """
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         """ Setter """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be >= 0")  
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
         """ Getter """
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         """ Setter """
@@ -75,3 +77,33 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """ Calculate area of a rectangle """
+        return self.__width * self.__height
+
+    def display(self):
+        """
+            That prints in stdout the Rectangle instance with
+            the character # by taking care of x and y
+        """
+        if self.__y > 0:
+            print("\n" * self.__y, end="")
+        for i in range(self.__height):
+            print(" " * self.__x, end="")
+            print("#" * self.__width)
+
+    def __str__(self):
+        """
+            method so that it returns
+            [Rectangle] (<id>) <x>/<y> - <width>/<height>
+        """
+
+        out = "[Rectangle] ({}) {}/{} - ".format(self.id, self.__x, self.__y)
+        out += "{}/{}".format(self.__width, self.__height)
+        return out
+
+    def update(self, *args):
+        atribute = ["id", "width", "height", "x", "y"]
+        for count, item in enumerate(args):
+            setattr(self, atribute[count], item)
