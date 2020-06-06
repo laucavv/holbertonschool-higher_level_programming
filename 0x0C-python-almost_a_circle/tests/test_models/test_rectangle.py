7ff8360aa24dd7ffa421ccb075ec57360c2235e6
laucavv <laucavv23@gmail.com>
2020-06-06 22:22:31 +0000

    update rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,10 +1,14 @@
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
-
 class TestClassRectangle(unittest.TestCase):
     def setUp(self):
-        self.rectangle = Rectangle(4,6,2,2)
+        self.rectangle = Rectangle(4, 6, 2, 2)
+        self.rectangle_1 = Rectangle(1, 7)
+        self.rectangle_2 = Rectangle(2, 4, 0, 3)
+        self.rectangle_3 = Rectangle(2, 5)
+
+
 
     def test_r0(self):
         """Normal Cases"""
@@ -37,6 +41,20 @@ class TestClassRectangle(unittest.TestCase):
         self.assertRaises(TypeError, Rectangle, 2, float("inf"), 8, 1)
         self.assertRaises(TypeError, Rectangle, 222, 197, "lau", 2398)
         self.assertRaises(TypeError, Rectangle)
+    def test_area(self):
+        self.assertEqual(self.rectangle.area(), 24)
+        self.assertEqual(self.rectangle_1.area(), 7)
+        self.assertEqual(self.rectangle_2.area(), 8)
+        self.assertEqual(self.rectangle_3.area(), 10)
+    def test_display(self):
+        pass
+    def test_str(self):
+        self.assertEqual(self.rectangle.__str__(), "[Rectangle] (46) 2/2 - 4/6")
+        self.assertEqual(self.rectangle_1.__str__(), "[Rectangle] (47) 0/0 - 1/7")
+        self.assertEqual(self.rectangle_2.__str__(), "[Rectangle] (48) 0/3 - 2/4")
+        self.assertEqual(self.rectangle_3.__str__(), "[Rectangle] (49) 0/0 - 2/5")
+
+
 
 if __name__ == '__main__':
     unittest.main()
