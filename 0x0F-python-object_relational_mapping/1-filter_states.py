5fab81e3bae6e3ce546fecfdc6e5e0f202fa3e89
laucavv <laucavv23@gmail.com>
2020-08-07 20:48:39 +0000

    lists all states with a name starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Script that lists all states from a database  hbtn_0e_0_usawith a name
+    starting with N
+"""
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db_date = {
+        'host': "localhost",
+        'port': 3306,
+        'user': argv[1],
+        'passwd': argv[2],
+        'db': argv[3]
+    }
+
+    db = MySQLdb.connect(**db_date)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE binary 'N%' ORDER BY id")
+
+    list_states = cursor.fetchall()
+    for states in list_states:
+        print(states)
+
+    cursor.close()
+    db.close()
