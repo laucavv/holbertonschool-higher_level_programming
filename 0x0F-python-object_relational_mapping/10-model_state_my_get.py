cd156542aacc9a4b5adffbb2fe39fd812325b47e
laucavv <laucavv23@gmail.com>
2020-08-08 21:16:27 +0000

    script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" script that prints the State object with the name passed as
+    argument from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    ssesion = Session()
+    result = ssesion.query(State).filter_by(name=argv[4]).all()
+    if result:
+        for date in result:
+            print("{}".format(date.id))
+    else:
+        print("Not found")
+
+    ssesion.close()
