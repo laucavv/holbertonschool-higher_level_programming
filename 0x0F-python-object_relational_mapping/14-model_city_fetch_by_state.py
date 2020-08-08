cc720d4df94015fe41dd67dec3d16b24ac655a97
laucavv <laucavv23@gmail.com>
2020-08-08 23:17:50 +0000

    change variable

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" script 14-model_city_fetch_by_state.py that prints all City
-    objects from the database hbtn_0e_14_usa
-"""
+"""script that lists all states from a database"""
 
 
 from sys import argv
@@ -12,13 +10,16 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    ssesion = Session()
-    result = ssesion.query(City, State).filter(City.state_id == State.id)
-    for state_c, city_c in result:
-        print("{}: ({}) {}".format(city_c.name, state_c.id, state_c.name))
-    ssesion.close()
+
+        engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost/{}'
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        data = Session()
+        results = data.query(City, State).filter(City.state_id == State.id)
+        for dac, das in results:
+                print("{}: ({}) {}".format(das.name, dac.id, dac.name))
+
+        data.close()
