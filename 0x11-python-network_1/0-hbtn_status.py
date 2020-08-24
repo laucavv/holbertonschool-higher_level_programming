7c5b63d86ca6199e9a727a6f0d29ca0eba29442c
laucavv <laucavv23@gmail.com>
2020-08-24 17:00:48 +0000

    status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status """
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status ') as response:
+        html = response.read()
+    print("Body response:")
+    print("    - type: {}".format(type(html)))
+    print("    - content: {}".format(html))
+    print("    - utf8 content: {}".format(html.decode('utf-8')))
