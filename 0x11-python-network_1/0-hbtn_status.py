32831d2fd1c034e5b0dca730c73072bb6e1ea1a8
laucavv <laucavv23@gmail.com>
2020-08-24 17:05:22 +0000

    fix pep8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status ') as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
     print("Body response:")
     print("    - type: {}".format(type(html)))
