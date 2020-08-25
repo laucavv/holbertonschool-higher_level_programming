9aeb11db916417d1e9de24b8fd2b593040e868a4
laucavv <laucavv23@gmail.com>
2020-08-25 00:50:38 +0000

    email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL and an email, sends a POST request
+    to the passed URL with the email as a parameter, and displays the body
+    of the response (decoded in utf-8)
+"""
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    values = {'email': argv[2]}
+    req = requests.post(url, data=values)
+    print(req.text)
