30fe247413b7bb9d89e440ab677d80c48fe7ddca
laucavv <laucavv23@gmail.com>
2020-08-25 02:51:24 +0000

    my token

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Python script that takes your Github credentials (username and password)
+    and uses the Github API to display your id
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    req = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    result = req.json
+    print(result.get('id'))
