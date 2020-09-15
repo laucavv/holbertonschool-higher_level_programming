32b331408e25e74cdcf01908cd7b27c689217d37
laucavv <laucavv23@gmail.com>
2020-09-15 16:26:28 -0500

    script that prints the first argument passed to it

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+  script that prints the first argument passed to it:
+*/
+let i = 0;
+while (process.argv[i]) {
+  i++;
+}
+if (i === 2) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
