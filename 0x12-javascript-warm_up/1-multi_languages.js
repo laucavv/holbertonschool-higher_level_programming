c15d5db87ff8c7e95459f1acf267f29f7789304f
laucavv <laucavv23@gmail.com>
2020-09-15 13:45:50 -0500

    script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* script that prints 3 lines:
+* The first line: "C is fun"
+* The second line: "Python is cool"
+* The third line: "Javascript is amazing"
+*/
+const myC = 'C is fun\n';
+const myPython = 'Python is cool\n';
+const myVar = 'Javascript is amazing';
+console.log(myC + myPython + myVar)
