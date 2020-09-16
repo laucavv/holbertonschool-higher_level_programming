5f13e4a984b9aa35fc896205d33db2c0502be2f8
laucavv <laucavv23@gmail.com>
2020-09-15 20:56:35 -0500

    fix style

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,9 +6,8 @@ const argv = process.argv;
 const numberArgv = argv.length;
 
 if (numberArgv <= 3) {
-  console.log('0')
-}
-else {
-  const secondb = argv.sort(function(a, b) { return (b - a);});
+  console.log('0');
+} else {
+  const secondb = argv.sort(function (a, b) { return (b - a); });
   console.log(secondb[3]);
 }
