e3c176557c247e5b5f47375a18c59daa0db727f5
laucavv <laucavv23@gmail.com>
2020-09-22 16:20:47 -0500

    script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/*  script that computes the number of tasks completed by user id.*/
+const request = require('request');
+const dict = {};
+request(process.argv[2], function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const info = JSON.parse(body);
+    let elemt;
+    for (elemt of info) {
+      if (elemt.completed) {
+        if (elemt.userId in dict) {
+          dict[elemt.userId] += 1;
+        } else {
+          dict[elemt.userId] = 1;
+        }
+      }
+    }
+    console.log(dict);
+  }
+}
+);
