81563848389307a27b42dc48412b60f178d947d9
laucavv <laucavv23@gmail.com>
2020-09-21 20:39:36 -0500

    fix style

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,5 +2,5 @@
 /* script that imports an array and computes a new array. */
 const list = require('./100-data.js').list;
 const newList = list.map((item, i) => item * i);
-console.log(list)
-console.log(newList)
+console.log(list);
+console.log(newList);
