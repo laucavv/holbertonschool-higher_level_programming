23dc0a7c8ff0888d5af7ad5ab4d5e75217ab8c6e
laucavv <laucavv23@gmail.com>
2020-09-22 14:19:36 -0500

    script that prints the number of movies where the character “Wedge Antilles” is present.

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/nodejs
+/*script that prints the number of movies where the character “Wedge Antilles” is present. */
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let count = 0;
+    let result = [];
+    let charac = [];
+    result = (JSON.parse(body).results);
+    for (let i = 0; i < result.length; i++) {
+      charac = result[i].characters;
+      charac.forEach(element => {
+        if (element.includes('18')) {
+          count++;
+        }
+      });
+    }
+    return count;
+  }
+});
