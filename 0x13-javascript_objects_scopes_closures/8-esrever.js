779c495f4242d6c80cdd79494443bffb934be838
laucavv <laucavv23@gmail.com>
2020-09-21 19:46:50 -0500

    returns the reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,5 +2,10 @@
 /* function that returns the reversed version of a list */
 
 exports.esrever = function (list) {
-  return list.reverse();
-}
+  const newA = [];
+  let i; let j = 0;
+  for (i = list.length; i > 0; i--, j++) {
+    newA[j] = list[i - 1];
+  }
+  return newA;
+};
