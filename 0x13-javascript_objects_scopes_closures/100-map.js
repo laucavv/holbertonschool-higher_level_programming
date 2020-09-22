988e5d6720ac6b55356b06620bd5a1b2dbe6892c
laucavv <laucavv23@gmail.com>
2020-09-21 20:38:37 -0500

    each value equal to the value of the initial list, multipled by the index in the list

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* script that imports an array and computes a new array. */
+const list = require('./100-data.js').list;
+const newList = list.map((item, i) => item * i);
+console.log(list)
+console.log(newList)
