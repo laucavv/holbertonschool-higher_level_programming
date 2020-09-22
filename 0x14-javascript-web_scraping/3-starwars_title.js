019502f4b89b253ac2f51ed6ca36e265dad8e050
laucavv <laucavv23@gmail.com>
2020-09-22 13:06:24 -0500

    prints the title of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* script that prints the title of a Star Wars movie where
+    the episode number matches a given integer.
+*/
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
