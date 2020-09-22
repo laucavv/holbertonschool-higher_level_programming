590ef8864feaef762d7aed6647838e9a036d50ee
laucavv <laucavv23@gmail.com>
2020-09-21 20:02:41 -0500

    prints the number of arguments already printed and the new argument value

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*  function that prints the number of arguments already printed and the new argument value */
+let num = 0;
+exports.logMe = function (item) {
+  console.log(num + ': ' + item);
+  num++;
+};
