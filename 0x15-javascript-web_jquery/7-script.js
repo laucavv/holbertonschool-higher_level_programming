3bc52943cebeb3e2822b6763f42bb20bad61fdbe
laucavv <laucavv23@gmail.com>
2020-09-24 18:09:43 -0500

    name

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,6 @@
+/* Javascript script that fetches and replaces the name of
+    this URL: https://swapi-api.hbtn.io/api/people/5/?format=json
+*/
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+  $('DIV#character').text(data.name);
+});
