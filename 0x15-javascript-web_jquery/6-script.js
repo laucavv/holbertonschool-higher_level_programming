f0e4c6d51ee6b948e73b5b696427d124e7d6acd7
laucavv <laucavv23@gmail.com>
2020-09-24 17:00:39 -0500

    new message

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,6 @@
+/* Javascript script that adds a LI element to a list when
+    the user clicks on the tag DIV#add_item
+*/
+$('DIV#update_header').click(function () {
+  $('HEADER').text('New Header!!!');
+});
